4216c2e8625b0094ddba31af82f6d85188b0fffc
Nicolas Marin Viegas <101991824+nicoymas@users.noreply.github.com>
2022-08-07 19:47:38 -0300

    Catálogo con MAPS y Promises

--- src/components/data/Data.jsx
+++ src/components/data/Data.jsx
@@ -3,73 +3,73 @@
 const DataPlantas=[ 
     {
     "id": 1,
-    "name": "Texasgrass",
-    "description": "Vaseyochloa multinervosa (Vasey) Hitchc.",
+    "name": "VIOLETA AFRICANA",
+    "description": "BLOOM GREEN CO.",
     "stock": 11,
     "precio": 455,
-    "img": "https://robohash.org/maximesintnesciunt.png?size=50x50&set=set1"
+    "img": "https://res.cloudinary.com/proyectoreact/image/upload/v1659911817/VIOLETA_AFRICANA_ul2hgn.jpg"
   }, {
     "id": 2,
-    "name": "Yellow Necklacepod",
-    "description": "Sophora tomentosa L. var. occidentalis (L.) Isely",
+    "name": "PALMERA DE BAMBÚ",
+    "description": "Esta planta tropical de origen oriental. Es de crecimiento muy rápido y se convierte en un recurso decorativo muy interesante para cualquier rincón de la casa.",
     "stock": 8,
     "precio": 327,
-    "img": "https://robohash.org/minimaremsint.png?size=50x50&set=set1"
+    "img": "https://res.cloudinary.com/proyectoreact/image/upload/v1659911816/DECOALIVE_nxtcfn.jpg"
   }, {
     "id": 3,
-    "name": "Roemer's False Indigo",
-    "description": "Amorpha roemeriana Scheele",
+    "name": "COSTILLA DE ADÁN",
+    "description": "También denominada monstera deliciosa, de la familia de las aráceas es una especie tropical del centro y sur de América. La Costilla de Adán es una de las plantas de interior que purifican el aire del dormitorio",
     "stock": 8,
     "precio": 425,
-    "img": "https://robohash.org/voluptasmollitiaesse.png?size=50x50&set=set1"
+    "img": "https://res.cloudinary.com/proyectoreact/image/upload/v1659911816/COSTILLA_DE_AD%C3%81N_cr6giy.jpg"
   }, {
     "id": 4,
-    "name": "Cottony Goldenaster",
-    "description": "Chrysopsis gossypina (Michx.) Elliott ssp. hyssopifolia (Nutt.) Semple",
+    "name": "CACTUS NATURALES",
+    "description": "La familia de las cactáceas es originaria del continente americano, y como tal, están acostumbradas a vivir en el desierto. Es por ello que aguantan tan bien las temperaturas extremas y la sequía.",
     "stock": 8,
     "precio": 168,
-    "img": "https://robohash.org/commodiquised.png?size=50x50&set=set1"
+    "img": "https://res.cloudinary.com/proyectoreact/image/upload/v1659911816/CACTUS_NATURALES_c3ordm.jpg"
   }, {
     "id": 5,
-    "name": "Keystone Hawthorn",
-    "description": "Crataegus tanuphylla Sarg.",
+    "name": "ANTURIO ROJO",
+    "description": "Esta planta tan bonita, florece durante casi todo el año, así que es, básicamente, un espectáculo visual. Sólo requiere luz indirecta y tierra húmeda. ",
     "stock": 20,
     "precio": 67,
-    "img": "https://robohash.org/nobisrepellatnam.png?size=50x50&set=set1"
+    "img": "https://res.cloudinary.com/proyectoreact/image/upload/v1659911816/ANTURIO_ROJO_wr0ffc.jpg"
   }, {
     "id": 6,
-    "name": "Seedbox",
-    "description": "Ludwigia alternifolia L.",
+    "name": "FICUS BENJAMINA",
+    "description": "Es una especie botánica de árbol de higo, nativa de Asia y Australia. Es el árbol oficial de Bangkok y puede llegar a 30 m de altura en condiciones naturales.",
     "stock": 2,
     "precio": 377,
-    "img": "https://robohash.org/doloribusliberout.png?size=50x50&set=set1"
+    "img": "https://res.cloudinary.com/proyectoreact/image/upload/v1659911816/FICUS_BENJAMINA_ld7q06.jpg"
   }, {
     "id": 7,
-    "name": "Phaeorrhiza Lichen",
-    "description": "Phaeorrhiza nimbosa (Fr.) H. Mayrh. & Poelt",
+    "name": "HARDY PILEA PEPEROMIOIDES",
+    "description": "Ofrece un follaje verde profundo, de formatos redondeados llamativos que transmiten frescura y elegancia. Necesita un riego periódico para que se mantenga con una bonita apariencia, especialmente en épocas de calor.",
     "stock": 7,
     "precio": 398,
-    "img": "https://robohash.org/sintaliasquisquam.png?size=50x50&set=set1"
+    "img": "https://res.cloudinary.com/proyectoreact/image/upload/v1659911816/HARDY_PILEA_PEPEROMIOIDES_wpnuta.jpg"
   }, {
     "id": 8,
-    "name": "Spathulate Candle Snuffer Moss",
-    "description": "Encalypta spathulata Müll. Hal.",
+    "name": "JAZMÍN DE MADAGASCAR",
+    "description": "Una bonita planta de interior con hojas redondeadas y gruesas, con flores de color blanco que van a dar un toque de elegancia a los interiores. Se cuida fácilmente y requiere riegos periódicos para que se mantenga frondosa y elegante.",
     "stock": 5,
     "precio": 74,
-    "img": "https://robohash.org/temporaeligendianimi.png?size=50x50&set=set1"
+    "img": "https://res.cloudinary.com/proyectoreact/image/upload/v1659911817/JAZM%C3%8DN_DE_MADAGASCAR_djhnd6.jpg"
   }, {
     "id": 9,
-    "name": "Fluxweed",
-    "description": "Trichostema brachiatum L.",
+    "name": "POTHO",
+    "description": "El potos es un miembro de la familia araceae, y es quizá una de las plantas más resistentes que existen, ya que puede crecer hasta en las peores condiciones. Además, tiene unas hojas preciosas que visten con elegancia cualquier estancia",
     "stock": 16,
     "precio": 231,
-    "img": "https://robohash.org/sintatqueid.png?size=50x50&set=set1"
+    "img": "https://res.cloudinary.com/proyectoreact/image/upload/v1659911817/POTHO_xlmqt1.jpg"
   }, {
     "id": 10,
-    "name": "Rose Of Sharon",
-    "description": "Hibiscus syriacus L.",
+    "name": "LAVANDA",
+    "description": "Si quieres darle un toque de color y originalidad a tu casa, nada mejor como la lavanda para que decore y proporcione ricos aromas que, incluso, pueden refrescar y enriquecer el ambiente.",
     "stock": 8,
     "precio": 196,
-    "img": "https://robohash.org/quosdebitisaliquam.png?size=50x50&set=set1"
+    "img": "https://res.cloudinary.com/proyectoreact/image/upload/v1659911817/LAVANDA_koymdl.jpg"
   }]
   export default DataPlantas;
